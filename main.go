57f38d709df6e200afda0b47e27bb16261b8c89d
Devesh Saini <57091455+deveshsainii@users.noreply.github.com>
2023-12-09 22:08:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// main.go
-
 package main
 
 import (
@@ -16,24 +14,22 @@ type RequestPayload struct {
 
 type ResponsePayload struct {
 	SortedArrays [][]int `json:"sorted_arrays"`
-	TimeNS      int64   `json:"time_ns"`
+	TimeNS       int64   `json:"time_ns"`
 }
 
 func processSingle(w http.ResponseWriter, r *http.Request) {
 	var reqPayload RequestPayload
 	if err := json.NewDecoder(r.Body).Decode(&reqPayload); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	startTime := time.Now()
 	sortedArrays := make([][]int, len(reqPayload.ToSort))
-
-	for i, subArray := range reqPayload.ToSort {
-		sort.Ints(subArray)
-		sortedArrays[i] = subArray
+	for i, arr := range reqPayload.ToSort {
+		sort.Ints(arr)
+		sortedArrays[i] = arr
 	}
-
 	timeTaken := time.Since(startTime).Nanoseconds()
 
 	response := ResponsePayload{
@@ -48,25 +44,22 @@ func processSingle(w http.ResponseWriter, r *http.Request) {
 func processConcurrent(w http.ResponseWriter, r *http.Request) {
 	var reqPayload RequestPayload
 	if err := json.NewDecoder(r.Body).Decode(&reqPayload); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	startTime := time.Now()
 	var wg sync.WaitGroup
 	sortedArrays := make([][]int, len(reqPayload.ToSort))
-
-	for i, subArray := range reqPayload.ToSort {
+	for i, arr := range reqPayload.ToSort {
 		wg.Add(1)
-		go func(i int, subArray []int) {
+		go func(i int, arr []int) {
 			defer wg.Done()
-			sort.Ints(subArray)
-			sortedArrays[i] = subArray
-		}(i, subArray)
+			sort.Ints(arr)
+			sortedArrays[i] = arr
+		}(i, arr)
 	}
-
 	wg.Wait()
-
 	timeTaken := time.Since(startTime).Nanoseconds()
 
 	response := ResponsePayload{
@@ -82,5 +75,6 @@ func main() {
 	http.HandleFunc("/process-single", processSingle)
 	http.HandleFunc("/process-concurrent", processConcurrent)
 
-	http.ListenAndServe(":8000", nil)
+	port := ":8000"
+	http.ListenAndServe(port, nil)
 }
